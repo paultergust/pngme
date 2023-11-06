fbf0333880abff4a31ef929fbde98d735c872f14
Paulo <paultergust@gmail.com>
2023-11-06 18:18:51 +0000

    fix: Fix function calls on png.c

--- c/png.c
+++ c/png.c
@@ -9,41 +9,41 @@ Png from_chunks(Vector chunks) {
   return png;
 }
 
-void append_chunk(Png* self, Chunk chunk) {
-  vector_append(&self->chunks, &chunk);
+void append_chunk(Png* png, Chunk chunk) {
+  vector_append(&png->chunks, &chunk);
 }
 
-int remove_chunk(Png* self, ChunkType *chunk_type) {
-  int index = chunk_by_type(self, chunk_type);
+int remove_chunk(Png* png, ChunkType *chunk_type) {
+  int index = chunk_by_type(png, chunk_type);
   if (index >= 0) {
-    return vector_delete(&self->chunks, index);
+    return vector_delete(&png->chunks, index);
   }
   return 0;
 }
 
-uint8_t *header(Png *self) {
-  return self->header;
+uint8_t *header(Png *png) {
+  return png->header;
 }
 
-Vector* chunks(Png* self) {
-  return &self->chunks;
+Vector* chunks(Png* png) {
+  return &png->chunks;
 }
 
-int chunk_by_type(Png* self, ChunkType *chunk_type) {
-  int total = self->chunks.total;
+int chunk_by_type(Png* png, ChunkType *chunk_type) {
+  int total = png->chunks.total;
   for (int i = 0; i < total; i++) {
-    if (compare_types(chunk_type, self->chunks.items[i])) {
+    if (compare_types(chunk_type, png->chunks.items[i])) {
       return i;
     }
   }
   return -1;
 }
 
-Vector as_bytes(Png* self) {
+Vector as_bytes(Png* png) {
   Vector vbytes;
   vector_init(&vbytes, 100);
-  for (int i = 0; i < self->chunks.total; i++) {
-    Chunk *c = self->chunks.items[i];
+  for (int i = 0; i < png->chunks.total; i++) {
+    Chunk *c = png->chunks.items[i];
     uint8_t len[4];
     uint32_to_uint8(c->length, len);
     vector_append(&vbytes, len);
