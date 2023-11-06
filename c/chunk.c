a5fb509e6d8cbd3940d8faf597947802ea6e6116
Paulo <paultergust@gmail.com>
2023-11-06 20:56:47 +0000

    feat: Create function to parse chunk from a vector of bytes

--- c/chunk.c
+++ c/chunk.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "helper.h"
 #include "chunk_type.h"
 #include "chunk.h"
 #include "crc_32.h"
+#include "vector.h"
 
 Chunk new_chunk(ChunkType type, Vector* data) {
   int length = data->total;
@@ -31,3 +33,43 @@ char* data_as_str(Chunk* chunk) {
   return (char*)chunk->data;
 }
 
+Chunk try_from_bytes(Vector *vec) {
+
+  //first 4 are the length
+  uint8_t len_bytes[4];
+  for (int a = 0; a < 4; a++) {
+    len_bytes[a] = (uint8_t)vec->items[a];
+  }
+  int length = uint8_to_uint32(len_bytes);
+
+  //now for the type
+  uint8_t type_bytes[4];
+  for (int b = 4; b < 8; b++) {
+    type_bytes[b] = (uint8_t)vec->items[b];
+  }
+  ChunkType type = from_bytes(type_bytes);
+
+  //then, the next (length) are the data
+  Vector data;
+  vector_init(&data, length);
+  for (int c = 4; c < length; c++) {
+    vector_append(&data, vec->items[c]);
+  }
+
+  //and then 4 for crc (which should match data)
+  uint8_t crc_bytes[4];
+  for (int d = 0; d < (length + 4); d++) {
+    crc_bytes[d] = (uint8_t)vec->items[d];
+  }
+  uint32_t crc = uint8_to_uint32(crc_bytes);
+  Chunk chunk = new_chunk(type, &data);
+  if (crc != chunk.crc) {
+    printf("Invalid crc for chunk: ");
+    for (int e = 0; e < 4; e++) {
+      printf("%d ", type_bytes[e]);
+    }
+    exit(1);
+  }
+  //if anything fails, throw an error
+  return chunk;
+}
